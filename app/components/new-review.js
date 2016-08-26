f78716c76cabc1060c9788b9c5450f3222c1bf1c
Chip Carnes <chip.carnes27@gmail.com>
2016-08-26 10:07:04 -0700

    Add review form and review tile on string page

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newReviewForm: false,
+
+  actions: {
+    showReviewForm() {
+      this.set('newReviewForm', true);
+    },
+
+    save() {
+      var params = {
+        name: this.get('name'),
+        date: new Date(),
+        title: this.get('title'),
+        content: this.get('content'),
+        rating: this.get('rating'),
+        string: this.get('string'),
+      };
+      this.set('newReviewForm', false);
+      this.sendAction('save', params);
+    }
+  }
 });
--- app/routes/string.js
+++ app/routes/string.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('string', params.string_id);
+  },
+
+  actions: {
+    save(params) {
+      var newReview = this.store.createRecord('review', params);
+      var string = params.string;
+      string.get('reviews').addObject(newReview);
+      newReview.save().then(function () {
+        return string.save();
+      });
+      this.transitionTo('string', params.string);
+    }
   }
 });
